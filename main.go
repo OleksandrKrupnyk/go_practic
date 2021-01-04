79a21853cfe2bb3711c0ce9d7d1c4da8c5088265
Alex Krupnik <krupnik_a@ukr.net>
2021-01-04 19:36:13 +0200

    List 1.2

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 /**
-List 1.1
+List 1.2
 */
 package main
 
 import "fmt"
 
-func Names() (string, string) {
-	return "Foo", "Bar"
+func Names() (first string, second string) {
+	first = "Maxim List 1.2"
+	second = "Aljona List 1.2"
+	return
 }
 
 func main() {
 	n1, n2 := Names()
 	fmt.Println(n1, n2)
-	n3, _ := Names()
-	fmt.Println(n3)
 }
