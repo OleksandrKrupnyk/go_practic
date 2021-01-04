3812b1ef50153f84520788a7144a09cf3926e999
Alex Krupnik <krupnik_a@ukr.net>
2021-01-04 21:58:15 +0200

    List 2.7

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package hello
+
+import "fmt"
+
+func getName() string {
+	return "World!"
+}
+
+func main() {
+	name := getName()
+	fmt.Println("Hello ", name)
+}
---
+++ hello/hello_test.go
@@ -0,0 +1,10 @@
+package hello
+
+import "testing"
+
+func TestName(t *testing.T) {
+	name := getName()
+	if name != "World!" {
+		t.Error("Response from getName is unexpected")
+	}
+}
