85a40967cc3215c8febfd0574c392c7e2daa019e
Alex Krupnik <krupnik_a@ukr.net>
2021-01-04 19:33:12 +0200

    List 1.1

---
+++ main.go
@@ -0,0 +1,17 @@
+/**
+List 1.1
+*/
+package main
+
+import "fmt"
+
+func Names() (string, string) {
+	return "Foo", "Bar"
+}
+
+func main() {
+	n1, n2 := Names()
+	fmt.Println(n1, n2)
+	n3, _ := Names()
+	fmt.Println(n3)
+}
