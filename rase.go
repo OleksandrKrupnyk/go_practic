2d17c6808ad76a387908d111b002cffee30c60e3
Alex Krupnik <krupnik_a@ukr.net>
2021-02-11 00:22:38 +0200

    List 3.5 Счетчик слов с состоянием гонки

---
+++ rase.go
@@ -0,0 +1,78 @@
+/*
+ List 3.5  Счетчик слов с состоянием гонки
+	$ go run ./rase.go ./exampledata/*.txt
+	Все горутины использую 1 общий объект структуру words что приводит к ошибку
+Проблема возникла в функции words.add. Несколько сопрограмм
+одновременно  обращаются  к  одному  и  тому  же  фрагменту  памяти,
+объекту words.found типа map
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+)
+
+// Структура для хранения хранения сло и количества их вхождения в файле
+type words struct {
+	found map[string]int
+}
+
+// создание нового экземпляра слова
+func newWords() *words {
+	return &words{found: map[string]int{}}
+}
+
+/**
+  Фиксируем количество вхождений n слова word
+*/
+func (w *words) add(word string, n int) {
+	count, ok := w.found[word]
+	// Если не найдено то создать
+	if !ok {
+		w.found[word] = n
+		return
+	}
+	// иначе добавить к существующему
+	w.found[word] = count + n
+}
+
+func tallyWords(filename string, dict *words) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		word := strings.ToLower(scanner.Text())
+		dict.add(word, 1)
+	}
+	return scanner.Err()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	w := newWords()
+	for _, f := range os.Args[1:] {
+		wg.Add(1)
+		go func(file string) {
+			if err := tallyWords(file, w); err != nil {
+				fmt.Println(err.Error())
+			}
+			wg.Done()
+		}(f)
+	}
+	wg.Wait()
+	fmt.Println("Words that appear more than once:") // Перед завершением
+	for word, count := range w.found {               //программы вывести
+		if count > 1 { //результаты поиска
+			fmt.Printf("%s: %d\n", word, count)
+		}
+	}
+
+}
